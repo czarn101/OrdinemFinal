e4e646bfb935f4323c653399b0b221560e885c18
thoma287 <Stevenisdrew@Drews-MacBook-Pro-6.local>
2017-01-19 10:01:06 -0800

    Verification/BE

--- FinalSubmission/Ordinem_iOS_fixed/Ordinem_iOS/PointView.swift
+++ FinalSubmission/Ordinem_iOS_fixed/Ordinem_iOS/PointView.swift
@@ -12,6 +12,32 @@ import UIKit
 public class PointView: UIViewController {
     
     @IBOutlet var pointBal: UILabel?
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var projectedAttendance: UITextField!
+    @IBOutlet weak var projectedCost: UILabel!
+    
+    @IBAction func stepperStepped(_ sender: UIStepper) {
+        pointBal!.text = String(Int(sender.value))
+    }
+    
+    var points = Double(1)
+    var attendance = Double(1)
+    
+    
+    func getPoints() -> Double{
+        self.points = Double(pointBal!.text!)!
+        return self.points
+    }
+    
+    func attToInt() -> Double{
+        self.attendance = Double(projectedAttendance.text!)!
+        return self.attendance
+    }
+
+    @IBAction func calcPressed(_ sender: UIButton) {
+        let total = Double(getPoints()*attToInt()*0.05*1.05)
+        projectedCost.text = String(total)
+    }
     
     let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
     
@@ -19,6 +45,55 @@ public class PointView: UIViewController {
         super.viewDidLoad()
         self.pointBal?.text = appDelegate.pointBalance
         //code
+        stepper.maximumValue = 99
+        stepper.stepValue = 1
+        // Do any additional setup after loading the view.
+        
+        
+        
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        
+        
+        let toolBar = UIToolbar()
+        
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(self.doneClicked))
+        
+        
+        toolBar.setItems([flexibleSpace, doneButton], animated: false)
+        
+        
+        projectedAttendance.inputAccessoryView = toolBar
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: nil)
+
+        
+    }
+    
+    @IBOutlet weak var theScrollView: UIScrollView!
+    
+    func keyboardWillShow(notification:NSNotification){
+        //give room at the bottom of the scroll view, so it doesn't cover up anything the user needs to tap
+        var userInfo = notification.userInfo!
+        var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+        
+        var contentInset:UIEdgeInsets = self.theScrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        self.theScrollView.contentInset = contentInset
+    }
+    
+    
+    func keyboardWillHide(notification:NSNotification){
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        self.theScrollView.contentInset = contentInset
+    }
+    
+    func doneClicked(){
+        view.endEditing(true)
     }
     
     override public func didReceiveMemoryWarning() {
